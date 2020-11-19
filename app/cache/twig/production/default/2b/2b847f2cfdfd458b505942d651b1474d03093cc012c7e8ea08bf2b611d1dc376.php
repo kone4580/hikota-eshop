7b59248f8665271e1888b6df29760e51262f9976
Kouichi Yokoyama <yokoyama@i-square.jp>
2020-11-19 20:02:08 +0900

    path変更

---
+++ app/cache/twig/production/default/2b/2b847f2cfdfd458b505942d651b1474d03093cc012c7e8ea08bf2b611d1dc376.php
@@ -0,0 +1,243 @@
+<?php
+
+/* Block/brands.twig */
+class __TwigTemplate_4cb2f0e3e762652b17b14575242bcd5f377bc69a6e19eb9eaeffbceaeed3f7e8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "    <section class=\"top-section-group\">
+      <div class=\"top-center-title\">
+        <h2>取り扱いブランド</h2>
+        <p class=\"eng\">BRNADS</p>
+      </div>
+
+      <ul class=\"brand_list mt50\">
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 11
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 20
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 29
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 38
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 47
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 56
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 65
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 74
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 83
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 92
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 101
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 110
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 119
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 128
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+
+        <li class=\"brand_list-item\">
+          <a href=\"#\">
+            <div class=\"brand_list-photo\">
+              <img src=\"";
+        // line 137
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+            </div>
+            <h4 class=\"brand_list-title\">オリオセタ</h4>
+          </a>
+        </li>
+      </ul>
+
+      <p class=\"more-btn\" id=\"more_btn\">View More</a>
+      <p class=\"more-btn\" id=\"close_btn\">Close</a>
+    </section>
+
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "Block/brands.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  199 => 137,  187 => 128,  175 => 119,  163 => 110,  151 => 101,  139 => 92,  127 => 83,  115 => 74,  103 => 65,  91 => 56,  79 => 47,  67 => 38,  55 => 29,  43 => 20,  31 => 11,  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "Block/brands.twig", "/Volumes/LaCie/MAMP/eccube-3/app/template/default/Block/brands.twig");
+    }
+}
---
+++ app/cache/twig/production/default/bb/bb4b7a777ae3ef7fa6faedf3b9236d0bd332f62ba8842717300fafe15fbb2829.php
@@ -0,0 +1,149 @@
+<?php
+
+/* Block/new_product.twig */
+class __TwigTemplate_345ae880bb1605eacfee0523e8e3483c12469e527c564e02cef69af7de676546 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<section class=\"top-section-group mt70\">
+  <div class=\"top-center-title\">
+    <h2>新着アイテム</h2>
+    <p class=\"eng\">NEW ARRIVAL</p>
+  </div>
+
+  <div class=\"sp-scroll\">
+    <div class=\"new_items mt50\">
+
+      <div class=\"product_list-item\">
+        <a href=\"/products/detail/2\">
+          <div class=\"product_list-photo\">
+            <div class=\"prod_img\">
+              <div class=\"imgContent\">
+                <img src=\"/html/upload/save_image/cafe-1.jpg\" alt=\"パーコレーター\">
+              </div>
+            </div>
+          </div>
+          <h4 class=\"product_list-title\">パーコレーター</h4>
+          <div class=\"recom_point\">
+            アウトドアでも淹れたてのコーヒーをお楽しみいただけます。いまだけ、おいしい淹れ方の冊子つきです。
+          </div>
+          <dl class=\"product_list-info\">
+            <dt>価格</dt>
+            <dd><p class=\"text1\">ログイン後に表示</p></dd>
+          </dl>
+        </a>
+      </div>
+
+      <div class=\"product_list-item\">
+        <a href=\"#\">
+          <div class=\"product_list-photo\">
+            <div class=\"prod_img\">
+              <div class=\"imgContent\">
+                <img src=\"";
+        // line 35
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+              </div>
+            </div>
+          </div>
+          <h4 class=\"product_list-title\">商品名がはいります。</h4>
+          <div class=\"recom_point\">
+            おすすめポイントがはいります。おすすめポイントがはいります。おすすめポイントがはいります。おすすめポイントが...
+          </div>
+          <dl class=\"product_list-info\">
+            <dt>価格</dt>
+            <dd><p class=\"text1\">ログイン後に表示</p></dd>
+          </dl>
+        </a>
+      </div>
+
+      <div class=\"product_list-item\">
+        <a href=\"#\">
+          <div class=\"product_list-photo\">
+            <div class=\"prod_img\">
+              <div class=\"imgContent\">
+                <img src=\"";
+        // line 55
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+              </div>
+            </div>
+          </div>
+          <h4 class=\"product_list-title\">商品名がはいります。</h4>
+          <div class=\"recom_point\">
+            おすすめポイントがはいります。おすすめポイントがはいります。おすすめポイントがはいります。
+          </div>
+          <dl class=\"product_list-info\">
+            <dt>価格</dt>
+            <dd><p class=\"text1\">ログイン後に表示</p></dd>
+          </dl>
+        </a>
+      </div>
+
+      <div class=\"product_list-item\">
+        <a href=\"#\">
+          <div class=\"product_list-photo\">
+            <div class=\"prod_img\">
+              <div class=\"imgContent\">
+                <img src=\"";
+        // line 75
+        echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["app"] ?? null), "config", array()), "front_urlpath", array()), "html", null, true);
+        echo "/assets/images/common/default.png\" alt=\"\">
+              </div>
+            </div>
+          </div>
+          <h4 class=\"product_list-title\">商品名がはいります。</h4>
+          <div class=\"recom_point\">
+            おすすめポイントがはいります。おすすめポイントがはいります。おすすめポイントがはいります。
+          </div>
+          <dl class=\"product_list-info\">
+            <dt>価格</dt>
+            <dd><p class=\"text1\">ログイン後に表示</p></dd>
+          </dl>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "Block/new_product.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  101 => 75,  78 => 55,  55 => 35,  19 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "Block/new_product.twig", "/Volumes/LaCie/MAMP/eccube-3/app/template/default/Block/new_product.twig");
+    }
+}
